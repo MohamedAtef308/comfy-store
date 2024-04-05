90b4854d3fd9b99e0d0af7657b2005b8d3c9ce9f
MohamedAtef308 <mimo3082002@gmail.com>
2024-04-05 13:23:49 +0200

    Products Pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+import { generateAmount } from "../utils";
 
 const PaginationContainer = () => {
-  return (
-    <div>PaginationContainer</div>
-  )
-}
+  const { pageCount, page } = useLoaderData().meta.pagination;
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
 
-export default PaginationContainer
+  const pages = generateAmount(pageCount);
+
+  const handlePageChange = (pageNumber) => {
+    const updatedSearch = new URLSearchParams(search);
+    updatedSearch.set("page", pageNumber);
+
+    const updatedUrl = `${pathname}?${updatedSearch.toString()}`;
+    navigate(updatedUrl);
+  };
+
+  if (pageCount < 2) return null;
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        {
+          // prev
+          page - 1 > 0 && (
+            <button
+              type="button"
+              onClick={() => {
+                handlePageChange(page - 1);
+              }}
+              className="btn btn-xs sm:btn-md join-item"
+            >
+              Prev
+            </button>
+          )
+        }
+
+        {/* numbers */}
+        {pages.map((pageNum) => (
+          <button
+            type="button"
+            onClick={() => {
+              handlePageChange(pageNum);
+            }}
+            className={`btn btn-xs sm:btn-md border-none join-item ${
+              pageNum === page && "bg-base-300 border-base-300"
+            }`}
+          >
+            {pageNum}
+          </button>
+        ))}
+
+        {
+          // next
+          page + 1 <= pageCount && (
+            <button
+              type="button"
+              onClick={() => {
+                handlePageChange(page + 1);
+              }}
+              className="btn btn-xs sm:btn-md join-item"
+            >
+              Next
+            </button>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default PaginationContainer;
